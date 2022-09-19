52a85ed246fb166979930246947d10d24cf2e6ad
Emjones18 <89420463+Emjones18@users.noreply.github.com>
2022-09-19 12:12:21 -0500

    DONE

--- src/lab/RoadTripLog.java
+++ src/lab/RoadTripLog.java
@@ -2,26 +2,33 @@ package lab;
 
 public class RoadTripLog {
 
+	private String TripName;
 	private double TotalDistance;
 	private double TotalFuel;
 	private double MPG;
 	
 	public RoadTripLog(String TripName) {
-		
-		public double addDistance(double moreDistance) {
-			TotalDistance += moreDistance;
-		}
-		public void addFuel(double moreFuel) {
-			TotalFuel += moreFuel;
-		}
-		public double getTotalDistance() {
-			return;
-		}
-		public double getTotalFuel() {
-			return;
-		}
-		public double getMGC() {
-			return;
-		}
+		this.TripName = TripName;
 	}
+	
+	
+	
+	public double addDistance(double moreDistance) {
+		this.TotalDistance += moreDistance;
+		return this.TotalDistance;
+	}
+	public double addFuel(double moreFuel) {
+		TotalFuel += moreFuel;
+		return moreFuel;
+	}
+	public double getTotalDistance() {
+		return TotalDistance; 
+	}
+	public double getTotalFuel() {
+		return TotalFuel;
+	}
+	public double getMPG() {
+		return MPG;
+	}
+	
 }
